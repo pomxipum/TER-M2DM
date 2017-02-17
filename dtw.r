cdbfa03bf5dd62d2af0784f2c1dbcb5c93645357
mversionpenny <margot.selosse@gmail.com>
2017-02-17 16:21:21 +0100

    dtw loop

--- dtw.r
+++ dtw.r
@@ -11,24 +11,34 @@ library(dtw)
 library(parallel)
 
 data <- read.csv("mat/2009_agg200.csv", header = T,sep = ",")
+save(data, file="data.RData")
+load("data.RData")
 data.matrix <- as.matrix(data)
-data.matrix.small <- data.matrix[1:10,1:100]
+data.matrix.small <- data.matrix[1:10,1:17000]
 
-computeDTW <- function(i){
-  template <- cos(data.matrix.small[i,])
-  try <- dtw(data.matrix.small[i,], template)
+computeDTW <- function(i,j){
+  try <- dtw(data.matrix.small[i,], data.matrix.small[j,])
+  return(try)
+}
+
+computeDist <- function(i,j){
+  try <- sqrt(sum((data.matrix[i,]-data.matrix[j,])^2))
   return(try)
 }
 
 # ncores = 6
 # hosts <- c("localhost", "rpi1", "rpi2", "rpi3", "rpi4", "rpi5")
 # cl <- makeCluster(rep(hosts, each=ncores/6), methods=F)
-cl <- makeCluster(detectCores()-1)
+cl <- makeCluster(detectCores()-2)
 
-clusterExport(cl, list("computeDTW","data.matrix.small","dtw"))
-time <- system.time(
-  res <- parLapply(cl, 1:(dim(data.matrix.small)[1]), fun = function(i) computeDTW(i))
+
+clusterExport(cl, list("computeDist","data.matrix.small","dtw"))
+time.dist <- system.time(
+  for(j in 1:(nrow(data.matrix.small)-1)){
+    res <- parLapply(cl, (j+1):(nrow(data.matrix.small)), fun = computeDTW, j=j)
+  }
 )
+
 res
 stopCluster(cl)
 
